aac6697876f585a4df228db90f6914845422a2ac
Eduardo Mandujano <jesusgutierrez191105@gmail.com>
2024-02-28 16:55:19 -0600

    DELETE script index

--- src/controllers/music.js
+++ src/controllers/music.js
@@ -1,12 +1,1 @@
-const playlistId = '09xU56kX9roem0nALF1oUh';
 
-<iframe
-  title="Spotify Embed: Recommendation Playlist "
-  src={`https://open.spotify.com/embed/playlist/09xU56kX9roem0nALF1oUh?utm_source=generator&theme=0`}
-  width="100%"
-  height="100%"
-  style={{ minHeight: '360px' }}
-  frameBorder="0"
-  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-  loading="lazy"
-/>
