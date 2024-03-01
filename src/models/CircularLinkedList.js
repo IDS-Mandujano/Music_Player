69dd1e23d3f79a35909573b33e3835b6b73d891a
Eduardo Mandujano <jesusgutierrez191105@gmail.com>
2024-03-01 11:19:03 -0600

    UPDATE class CircularLinkedList

--- src/models/CircularLinkedList.js
+++ src/models/CircularLinkedList.js
@@ -1,77 +1,77 @@
-import { Node } from "./Node.js";
+import { Node } from "./Node.js"
 
 export class CircularLinkedList {
     constructor() {
-      this.head = null;
-      this.current = null;
-      this.isPlaying = false;
+      this.head = null
+      this.current = null
+      this.isPlaying = false
     }
 
     push(data, source) {
-        const newNode = new Node(data, source);
+        const newNode = new Node(data, source)
         if (!this.head) {
-          this.head = newNode;
-          this.head.next = this.head;
-          this.current = this.head;
+          this.head = newNode
+          this.head.next = this.head
+          this.current = this.head
         } else {
-          newNode.next = this.head;
-          let current = this.head;
+          newNode.next = this.head
+          let current = this.head
           while (current.next !== this.head) {
-            current = current.next;
+            current = current.next
           }
-          current.next = newNode;
+          current.next = newNode
         }
       }
 
     play() {
-        const audioPlayer = document.getElementById('audioPlayer');
-        const progressBar = document.getElementById('progressBar');
+        const audioPlayer = document.getElementById('audioPlayer')
+        const progressBar = document.getElementById('progressBar')
         
         if (this.current && this.current.source) {
-          console.log('Reproduciendo:', this.current.data);
-          audioPlayer.src = this.current.source;
-          audioPlayer.play();
-          this.isPlaying = true;
+          console.log('Reproduciendo:', this.current.data)
+          audioPlayer.src = this.current.source
+          audioPlayer.play()
+          this.isPlaying = true
     
           audioPlayer.addEventListener('timeupdate', function () {
-            const percent = (audioPlayer.currentTime / audioPlayer.duration) * 100;
-            progressBar.style.width = percent + '%';
-            progressBar.setAttribute('aria-valuenow', percent);
-          });
+            const percent = (audioPlayer.currentTime / audioPlayer.duration) * 100
+            progressBar.style.width = percent + '%'
+            progressBar.setAttribute('aria-valuenow', percent)
+          })
         } else {
-          console.log('La lista está vacía o la fuente es indefinida');
+          console.log('La lista está vacía o la fuente es indefinida')
         }
       }
     
       pause() {
-        const audioPlayer = document.getElementById('audioPlayer');
-        audioPlayer.pause();
-        this.isPlaying = false;
-        console.log('Pausado:', this.current.data);
+        const audioPlayer = document.getElementById('audioPlayer')
+        audioPlayer.pause()
+        this.isPlaying = false
+        console.log('Pausado:', this.current.data)
       }
     
       resume() {
-        const audioPlayer = document.getElementById('audioPlayer');
-        audioPlayer.play();
-        this.isPlaying = true;
-        console.log('Reanudando:', this.current.data);
+        const audioPlayer = document.getElementById('audioPlayer')
+        audioPlayer.play()
+        this.isPlaying = true
+        console.log('Reanudando:', this.current.data)
       }
     
       next() {
         if (this.current) {
-          this.current = this.current.next;
-          this.play();
+          this.current = this.current.next
+          this.play()
         }
       }
     
       previous() {
         if (this.current) {
-          let temp = this.head;
+          let temp = this.head
           while (temp.next !== this.current) {
-            temp = temp.next;
+            temp = temp.next
           }
-          this.current = temp;
-          this.play();
+          this.current = temp
+          this.play()
         }
       }
 }
--- src/models/Node.js
+++ src/models/Node.js
@@ -1,7 +1,7 @@
 export class Node {
     constructor(data, source) {
-      this.data = data;
-      this.source = source;
-      this.next = null;
+      this.data = data
+      this.source = source
+      this.next = null
     }
   }
