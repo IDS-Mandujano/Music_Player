8b62bc196d62ea6a198a6319a53b2082afd1c5b3
Eduardo Mandujano <jesusgutierrez191105@gmail.com>
2024-03-01 08:57:37 -0600

    DELETE linkedList and DoublyLinkedList

--- src/controllers/dependencies.js
+++ src/controllers/dependencies.js
@@ -1,15 +1,7 @@
-import { DoublyLinkedList } from "../models/DoublyLinkedList/DoublyLinkedList.js";
-import { LinkedList } from "../models/LinkedList/LinkedList.js";
-import { CircularLinkedList } from "../models/CircularLinkedList/CircularLinkedList.js"
-
+import { CircularLinkedList } from "../models/CircularLinkedList.js"
 const customEqualsFn = (a, b) => {
     return a === b;
 }
 
-const list = new LinkedList(customEqualsFn);
-const doublyList = new DoublyLinkedList(customEqualsFn)
 const circularLinkedList = new CircularLinkedList(customEqualsFn)
-
-export { list };
-export { doublyList }
 export { circularLinkedList }
